ac74d9453ee52860bf6bc49a321ef486645e5669
emmapejko <emma.pejko@gmail.com>
2021-10-05 17:44:09 -0500

    feat: working bar chart organized by activity type

--- client/src/components/Pages/ActivityLog.jsx
+++ client/src/components/Pages/ActivityLog.jsx
@@ -1,35 +1,29 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
+import * as d3 from 'd3';
 
 import BarChart from '../charts/BarChart.jsx';
 
-const datas = [
-  [10, 30, 40, 20],
-  [10, 40, 30, 20, 50, 10],
-  [60, 30, 40, 20, 30],
-];
-
-let i = 0;
-
 const ActivityLog = ({ user, events }) => {
   const [data, setData] = useState([]);
-  const [categories, setCategories] = useState([]);
-
-  // const changeData = () => {
-  //   setData(datas[i++]);
-  //   if (i === datas.length) i = 0;
-  // };
 
   const getUserStatsByCategory = () => {
-    const dataObj = {};
+    const activityData = [];
+    const eventObj = {};
 
     events.forEach(event => {
       if (event.attendees.includes(`${user.firstName} ${user.lastName}`)) {
-        dataObj[event.activity] = dataObj[event.activity] ? dataObj[event.activity] + 1 : 1;
+        eventObj[event.activity] = eventObj[event.activity] ? eventObj[event.activity] + 1 : 1;
       }
     });
 
-    setData(Object.values(dataObj));
-    setCategories(Object.keys(dataObj));
+    Object.keys(eventObj).forEach(activity => {
+      //activityData.push({ activity: activity, amount: eventObj[activity]});
+      activityData.push([ activity, eventObj[activity] ]);
+    });
+
+    console.log(activityData);
+    setData(activityData);
   };
 
   useEffect(() => {
@@ -38,18 +32,9 @@ const ActivityLog = ({ user, events }) => {
 
   return (
     <div>
-      {/* <div>
-        {
-          events.map((event, i) => (event.attendees.includes(`${user.firstName} ${user.lastName}`)
-            ? <div key={i}>{event.activity}</div>
-            : null))
-        }
-      </div> */}
       <div>
-        {/* <button type="button" onClick={changeData}>Change Data</button> */}
-        <BarChart width={600} height={600} data={data} categories={categories} />
+        <BarChart width={600} height={600} eventData={data} yAxisAttribute={'amount'} xAxisAttribute={'activity'}/>
       </div>
-
     </div>
   );
 };
--- client/src/components/charts/BarChart.jsx
+++ client/src/components/charts/BarChart.jsx
@@ -1,80 +1,113 @@
+/* eslint-disable */
+
 import * as d3 from 'd3';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { Chart } from 'react-google-charts';
 
-const BarChart = ({ width, height, data, categories }) => {
-  const ref = useRef();
-
-  const draw = () => {
-    const svg = d3.select(ref.current);
-    const selection = svg.selectAll('rect').data(data);
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(data)])
-      .range([0, height - 100]);
-    const xScale = d3.scaleLinear()
-      .domain(categories);
-    const yAxis = d3.axisLeft()
-      .scale(yScale);
-
-    svg.append('text')
-      .attr('transform', 'translate(100, 0)')
-      .attr('x', 50)
-      .attr('y', 50)
-      .attr('font-size', '24px')
-      .text('Events Attended by Activity Type');
-
-    selection
-      .transition().duration(300)
-      .attr('height', (d) => yScale(d))
-      .attr('y', (d) => height - yScale(d));
-
-    selection
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => i * 45)
-      .attr('y', (d) => height)
-      .attr('width', 40)
-      .attr('height', 0)
-      .attr('fill', 'orange')
-      .transition()
-      .duration(300)
-      .attr('height', (d) => yScale(d))
-      .attr('y', (d) => height - yScale(d));
-
-    selection
-      .exit()
-      .transition().duration(300)
-      .attr('y', (d) => height)
-      .attr('height', 0)
-      .remove();
-
-    selection.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(xScale))
-      .append('text')
-      .attr('y', height - 250)
-      .attr('x', width - 100)
-      .attr('text-anchor', 'end')
-      .attr('stroke', 'black')
-      .text('Activity');
-  };
-
-  useEffect(() => {
-    const svg = d3.select(ref.current)
-      .attr('width', width)
-      .attr('height', height)
-      .style('border', '1px solid black');
-  }, []);
-
-  useEffect(() => {
-    draw();
-  }, [data]);
-
-  return (
-    <div className="barChart" style={{ margin: '20px' }}>
-      <svg ref={ref} />
-    </div>
-
-  );
-};
+const BarChart = ({ eventData }) => (
+  <Chart
+    width={'600px'}
+    height={'400px'}
+    chartType="BarChart"
+    loader={<div>Loading Chart</div>}
+    data={[
+      [
+        'Activity',
+        'Number of Events',
+      ],
+      ...eventData
+    ]}
+    options={{
+      title: 'Events Attended by Activity Type',
+      width: 600,
+      height: 400,
+      bar: { groupWidth: '95%' },
+      legend: { position: 'none' },
+    }}
+  />
+)
 
 export default BarChart;
+
+// const BarChart = ({width, height, data, categories}) => {
+//   const ref = useRef();
+
+//   const draw = () => {
+//     const svg = d3.select(ref.current);
+//     const selection = svg.selectAll('rect').data(data);
+//     const yScale = d3.scaleLinear()
+//       .domain([0, d3.max(data)])
+//       .range([0, height - 100]);
+//     const xScale = d3.scaleLinear()
+//       .domain(categories);
+//     const yAxis = d3.axisLeft()
+//       .scale(yScale);
+
+//     svg.append('text')
+//       .attr('transform', 'translate(100, 0)')
+//       .attr('x', 50)
+//       .attr('y', 50)
+//       .attr('font-size', '24px')
+//       .text('Events Attended by Activity Type');
+
+//     selection
+//       .transition().duration(300)
+//       .attr('height', (d) => yScale(d))
+//       .attr('y', (d) => height - yScale(d));
+
+//     selection
+//       .enter()
+//       .append('rect')
+//       .attr('x', (d, i) => i * 45)
+//       .attr('y', (d) => height)
+//       .attr('width', 40)
+//       .attr('height', 0)
+//       .attr('fill', 'orange')
+//       .transition()
+//       .duration(300)
+//       .attr('height', (d) => yScale(d))
+//       .attr('y', (d) => height - yScale(d))
+
+//     selection
+//       .exit()
+//       .transition().duration(300)
+//       .attr('y', (d) => height)
+//       .attr('height', 0)
+//       .remove();
+
+//     selection.append('g')
+//       .attr('transform', `translate(0,${height})`)
+//       .call(d3.axisBottom(xScale))
+//       .append('text')
+//       .attr('y', height - 250)
+//       .attr('x', width - 100)
+//       .attr('text-anchor', 'end')
+//       .attr('stroke', 'black')
+//       .text('Activity');
+//   };
+
+//   useEffect(() => {
+//     const svg = d3.select(ref.current)
+//       .attr('width', width)
+//       .attr('height', height)
+//       .style('border', '1px solid black');
+//   }, []);
+
+//   useEffect(() => {
+//     draw();
+//   }, [data]);
+
+//   return (
+//     <div className="barChart" style={{ margin: '20px' }}>
+//       <svg ref={ref} />
+//       {
+//         categories.map((category, i) => <div key={i}>{category}</div>)
+//       }
+//     </div>
+
+//   );
+
+// };
+
+
+// export default BarChart;
